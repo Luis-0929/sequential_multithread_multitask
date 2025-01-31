154410a574c0ade80fcfc0f62dc3467673ce0746
Luis Gallegos <luisrgallegos99@gmail.com>
2025-01-30 20:01:15 -0800

    Added functionality to hae large int using __int128

--- sequential.c
+++ sequential.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdint.h>
+
+
+void print_int128(__int128 num) {
+    if (num > 9) {
+        print_int128(num / 10);
+    }
+    putchar('0' + (num % 10));
+}
 
 int main() {
     clock_t start, end;
     long long int n_1 = 100000000;
     long long int n_2 = 1000000000; 
-    
     long long int n_3 = 10000000000;
     
     long long int sum_1 = 0;
     long long int sum_2 = 0; 
-    
-    unsigned long long int sum_3 = 0;
-    unsigned long long int sum_4 = 0;
-    unsigned long long int sum_5 = 0;
+    __int128 sum_3 = 0;
 
 
 
@@ -24,8 +29,6 @@ int main() {
     end = clock();
     double elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("The sum of n is %lld and the time elapsed for that workload was %.10f seconds\n", sum_1, elapsed);
-
-
 
 
     start = clock();
@@ -40,17 +43,12 @@ int main() {
 
 
     start = clock();
-    for(long long int i = 0; i < (n_3 / 2); i++) {
-        sum_3 += i;
+    for(__int128 i = 0; i < n_3; i++) {
+         sum_3 += i;
     }
-    for(long long int i = 0; i < (n_3 / 2); i++) {
-        sum_4 += i;
-    }
-    sum_5 = sum_3 + sum_4;
-    
     end = clock();
     elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("The sum of n_3 is %lld and the time elapsed for that workload was %.10f seconds\n", sum_5, elapsed);
+    printf("The sum of n_3 is "); print_int128(sum_3); printf(" and the time elapsed for that workload was %.10f seconds\n", elapsed);
 
     return 0;
 }
